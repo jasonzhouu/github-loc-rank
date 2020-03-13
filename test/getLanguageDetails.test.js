2084a110a1563954ea0efd9544fc003d09b82dd1
Yusheng Zhou 周宇盛 <jasonzhouu@gmail.com>
2020-03-13 12:41:02 +0800

    将getLanguageDetails的代码移到githb-loc的项目中

--- test/getLanguageDetails.test.js
+++
@@ -1,7 +0,0 @@
-const getLanguageDetails = require('../getLanguageDetails.js');
-
-(async function IIFE() {
-  console.log(
-    await getLanguageDetails('bitcoin/bitcoin', '7420af224d5e2034e4f07f338f594000a88460e8'),
-  );
-}());
