660321da7e99d133883711465616d3bb3e642444
Yusheng Zhou 周宇盛 <jasonzhouu@gmail.com>
2020-03-14 07:31:17 +0800

    防止数据结构不是预期的样子

--- parseHeaderLink.js
+++ parseHeaderLink.js
@@ -1,11 +1,14 @@
 module.exports = function parseHeaderLink(link) {
-  // const link = '<https://api.github.com/user/19926174/starred?sort=created&page=2>; rel="next", <https://api.github.com/user/19926174/starred?sort=created&page=72>; rel="last"';
+  // todo: 如果没有第二页，headers.link会是怎么样的
+  let pageLength = 0;
 
-  const linkArr = link.split('/');
+  // 防止数据结构不是预期的样子
+  try {
+    pageLength = link.split('/')[8].split('=')[2].slice(0, -6);
+  } catch (error) {
+    pageLength = 1;
+    console.error(error);
+  }
 
-  const pageLength = linkArr[8].split('=')[2].slice(0, -6);
-
-  return {
-    pageLength,
-  };
+  return pageLength;
 };
