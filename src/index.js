451c60fe1bf84b3b592e29bc118070e1ae658ec1
Yusheng Zhou 周宇盛 <jasonzhouu@gmail.com>
2020-03-14 13:09:04 +0800

    next page button, show loadding while loadding data

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@
  * 3。用户输入token
  * 4。设置排序按钮
  * 5。设置筛选功能
- * 6。webpack 打包代码，因为应用的包用的是commonjs模块系统，需要进行转换
- * 7。webpack 自动打包，自动刷新页面
+ * √ 6。webpack 打包代码，因为应用的包用的是commonjs模块系统，需要进行转换
+ * √ 7。webpack 自动打包，自动刷新页面
  */
 const StarredRepositories = require('github-loc-rank');
 
-const token = 'fa398496dfec81c1d6c447d46be8c0d284a2405e';
+const token = 'baba1459cfd2b8b86d806c5e0211fa3166e06264';
 const starredRepositories = new StarredRepositories({ token });
 
-(async () => {
-  await starredRepositories.init();
+// todo: 边加载边渲染
+function render() {
   starredRepositories.get().forEach((repository) => {
     const row = document.createElement('tr');
     const reponame = document.createElement('td');
@@ -32,4 +32,27 @@ const starredRepositories = new StarredRepositories({ token });
     row.append(reponame, mainLanguage, loc);
     document.querySelector('table').append(row);
   });
+}
+
+(async () => {
+  await starredRepositories.init();
+  render();
 })();
+
+document.querySelector('body button').addEventListener('click', async (event) => {
+  // done: 加载时提示正在加载
+  event.target.style.display = 'none';
+  const loadding = document.querySelector('#loadding');
+  loadding.textContent = 'loadding';
+
+  const lastPage = await starredRepositories.getOnePage();
+
+  render();
+
+  event.target.style.display = 'inline';
+  loadding.textContent = '';
+
+  if (lastPage === true) {
+    event.target.style.display = 'none';
+  }
+});
