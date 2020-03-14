c66fcbd4927bf9ead8088b5069f7a01bb6689e13
Yusheng Zhou 周宇盛 <jasonzhouu@gmail.com>
2020-03-14 15:02:28 +0800

    按照loc排序； 样式；

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@
  * √ 1。获取数据
  * √ 2。展示数据
  * √ 3。用户输入token
- * 4。设置排序按钮
+ * √ 4。设置排序功能
  * 5。设置筛选功能
  * √ 6。webpack 打包代码，因为应用的包用的是commonjs模块系统，需要进行转换
  * √ 7。webpack 自动打包，自动刷新页面
  */
 const StarredRepositories = require('github-loc-rank');
 
-// let token = 'baba1459cfd2b8b86d806c5e0211fa3166e06264';
-// const starredRepositories = new StarredRepositories({ token });
 let token;
 let starredRepositories;
 
@@ -21,21 +19,23 @@ document.querySelector('#token').addEventListener('click', async (event) => {
 
   token = document.querySelector('input').value;
   starredRepositories = new StarredRepositories({ token });
-  await starredRepositories.init();
-  render();
-  status.textContent = '√';
+  const pageLength = await starredRepositories.init();
+  status.textContent = `√ there are ${pageLength} pages`;
 });
 
 // todo: 边加载边渲染
 function render() {
+  document.querySelector('tbody').innerHTML = '';
   starredRepositories.get().forEach((repository) => {
     const row = document.createElement('tr');
     const reponame = document.createElement('td');
     const mainLanguage = document.createElement('td');
     const loc = document.createElement('td');
+    const stars = document.createElement('td');
 
     reponame.textContent = repository.htmlUrl;
     loc.textContent = repository.loc;
+    stars.textContent = repository.stars;
 
     if (repository.mainLanguage === null) {
       mainLanguage.textContent = '-';
@@ -43,15 +43,16 @@ function render() {
       mainLanguage.textContent = repository.mainLanguage;
     }
 
-    row.append(reponame, mainLanguage, loc);
-    document.querySelector('table').append(row);
+    row.append(reponame, mainLanguage, stars, loc);
+    document.querySelector('tbody').append(row);
   });
 }
 
-document.querySelector('#nextPage').addEventListener('click', async (event) => {
+document.querySelector('#load').addEventListener('click', async (event) => {
+  const loadding = document.querySelector('#loadding');
+
   // done: 加载时提示正在加载
   event.target.style.display = 'none';
-  const loadding = document.querySelector('#loadding');
   loadding.textContent = 'loadding';
 
   const lastPage = await starredRepositories.getOnePage();
@@ -64,4 +65,20 @@ document.querySelector('#nextPage').addEventListener('click', async (event) => {
   if (lastPage === true) {
     event.target.style.display = 'none';
   }
+});
+
+document.getElementById('loc').addEventListener('click', (event) => {
+  if (!starredRepositories) { return; }
+  if (event.target.getAttribute('aria-sort') === 'none' || event.target.getAttribute('aria-sort') === 'ascending') {
+    starredRepositories.sort('loc', 'descending');
+    event.target.setAttribute('aria-sort', 'descending');
+    event.target.querySelector('span').classList.add('descending');
+    event.target.querySelector('span').classList.remove('ascending');
+  } else if (event.target.getAttribute('aria-sort') === 'descending') {
+    starredRepositories.sort('loc', 'ascending');
+    event.target.setAttribute('aria-sort', 'ascending');
+    event.target.querySelector('span').classList.add('ascending');
+    event.target.querySelector('span').classList.remove('descending');
+  }
+  render();
 });
