37aa826e2b7e67038b1d8f6ab0541f9119902a29
Yusheng Zhou 周宇盛 <jasonzhouu@gmail.com>
2020-03-14 13:09:36 +0800

    parse headers

--- parseHeaderLink.js
+++ parseHeaderLink.js
@@ -4,7 +4,7 @@ module.exports = function parseHeaderLink(link) {
 
   // 防止数据结构不是预期的样子
   try {
-    pageLength = link.split('/')[8].split('=')[2].slice(0, -6);
+    [pageLength] = link.split('/')[8].split('=')[1].split('>');
   } catch (error) {
     pageLength = 1;
     console.error(error);
